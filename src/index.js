18314f320caccef9f64b3acf5d7136c9c95c6509
Elham Rahmati <elhamrahmati1986@gmail.com>
2023-06-05 11:12:40 +0200

    Modify index.js file

--- src/index.js
+++ src/index.js
@@ -108,6 +108,9 @@ var arr = [1, 1/2, 1/4, 1/8, 1/16, 1/32];
   console.log(`result of async_sum is ready: ${result}`);
   tot = sum(arr)
   console.log(`result of sum is ready: ${tot}`);
+    console.log(`result of async_sum is ready: ${result}`);
+  tot = sum(arr)
+  console.log(`result of sum is ready: ${tot}`);
 })()
 
 
